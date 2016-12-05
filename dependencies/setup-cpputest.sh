7f7bbf32850b238490d37bb8a68907c75b56a999
Felipe Lavratti <felipelav@gmail.com>
2016-12-05 18:56:52 -0200

    Changed tree structure, updated ci script.

--- dependencies/setup-cpputest.sh
+++ dependencies/setup-cpputest.sh
@@ -3,6 +3,7 @@
 # Bash script that download and build cpputest inside the project tree,
 # where project's Makefile will look for it.
 
+rm -rvf cpputest-3.8
 rm cpputest-3.8.tar.gz
 wget https://github.com/cpputest/cpputest/releases/download/v3.8/cpputest-3.8.tar.gz
 tar -xf cpputest-3.8.tar.gz
