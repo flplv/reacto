9e8198e022b4b394e159876678dc9b0dc5b26161
Felipe Lavratti <felipelav@gmail.com>
2016-12-06 03:04:28 -0200

    Fixed dependencies of cpputest -include files

--- dependencies/cpputest/setup-cpputest.sh
+++ dependencies/cpputest/setup-cpputest.sh
@@ -8,16 +8,16 @@ if [ -z "$1" ]; then
 fi
 
 if [ -e $1 ]; then
-    if [ -d $1 ]; then
+    if [ ! -d $1 ]; then
         echo "Path $1 exists and is not a folder."
         exit -1
     fi
 fi
 
 rm -rf $1
-rm cpputest-3.8.tar.gz
 
-wget https://github.com/cpputest/cpputest/releases/download/v3.8/cpputest-3.8.tar.gz
+if [ ! -e './cpputest-3.8.tar.gz' ]; then
+    wget https://github.com/cpputest/cpputest/releases/download/v3.8/cpputest-3.8.tar.gz
+fi
 tar -xf cpputest-3.8.tar.gz
 mv cpputest-3.8 $1
-rm cpputest-3.8.tar.gz
